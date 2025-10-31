5ff986cad4b83cb62d2c7ed3b011883d890e6084
Minahil Fatima <meerubawan471@gmail.com>
2025-10-31 22:21:06 +0500

    Add files via upload

---
+++ main.mjs
@@ -0,0 +1,18 @@
+// main.js
+import { students } from "./student.mjs";
+import { teachers } from "./teacher.mjs";
+
+console.log("Student Information:");
+students.forEach((student) => {
+  const status = student.marks >= 70 ? "Pass" : "Fail";
+  console.log(
+    `Name: ${student.name}, Subject: ${student.subject}, Marks: ${student.marks}, Status: ${status}`
+  );
+});
+
+console.log("\nTeacher Information:");
+teachers.forEach((teacher) => {
+  console.log(
+    `Name: ${teacher.name}, Subject: ${teacher.subject}, Experience: ${teacher.experience}`
+  );
+});
---
+++ student.mjs
@@ -0,0 +1,7 @@
+// student.mjs
+export const students = [
+  { id: 1, name: "Ali Khan", marks: 85, subject: "Mathematics" },
+  { id: 2, name: "Amna Khan", marks: 65, subject: "Science" },
+  { id: 3, name: "Fatima Khan", marks: 75, subject: "English" },
+  { id: 4, name: "Asad Khan", marks: 95, subject: "Computer Science" },
+];
