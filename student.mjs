8e85ee72a7b8b7bc320991624ba934714aa2c50f
Minahil Fatima <meerubawan471@gmail.com>
2025-10-31 22:22:41 +0500

    Update student.mjs

--- student.mjs
+++ student.mjs
@@ -1,7 +1,9 @@
-// student.mjs
+// Task 1
+//student.mjs
 export const students = [
   { id: 1, name: "Ali Khan", marks: 85, subject: "Mathematics" },
   { id: 2, name: "Amna Khan", marks: 65, subject: "Science" },
   { id: 3, name: "Fatima Khan", marks: 75, subject: "English" },
   { id: 4, name: "Asad Khan", marks: 95, subject: "Computer Science" },
 ];
+
