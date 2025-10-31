1c52730781b347a68455e68b88be0a665a9e5312
Minahil Fatima <meerubawan471@gmail.com>
2025-10-31 22:23:05 +0500

    Update teacher.mjs

--- teacher.mjs
+++ teacher.mjs
@@ -1,6 +1,8 @@
 // teacher.mjs
+// Task 1
 export const teachers = [
   { id: 1, name: "Sir Ahmed", subject: "Mathematics", experience: "5 Years" },
   { id: 2, name: "Miss Sara", subject: "Science", experience: "3 Years" },
   { id: 3, name: "Sir Bilal", subject: "Computer Science", experience: "7 Years" },
 ];
+
